da5f73d5b7ff404f0a99b98b577ff37467f2dcb7
Pei Pei <Pei.Pei@D2L.com>
2015-09-16 22:06:30 -0400

    be able to access twitter account

--- TwitterInstant-Starter-4/TwitterInstant/RWSearchFormViewController.m
+++ TwitterInstant-Starter-4/TwitterInstant/RWSearchFormViewController.m
@@ -8,12 +8,26 @@
 
 #import "RWSearchFormViewController.h"
 #import "RWSearchResultsViewController.h"
+#import <ReactiveCocoa.h>
+#import <Accounts/Accounts.h>
+#import <Social/Social.h>
+
+typedef NS_ENUM(NSInteger, RWTwitterInstantError) {
+    RWTwitterInstantErrorAccessDenied,
+    RWTwitterInstantErrorNoTwitterAccounts,
+    RWTwitterInstantErrorInvalidResponse
+};
+
+static NSString * const RWTwitterInstantDomain = @"TwitterInstant";
 
 @interface RWSearchFormViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextField *searchText;
 
 @property (strong, nonatomic) RWSearchResultsViewController *resultsViewController;
+
+@property (strong, nonatomic) ACAccountStore *accountStore;
+@property (strong, nonatomic) ACAccountType *twitterAccountType;
 
 @end
 
@@ -29,6 +43,41 @@
   
   self.resultsViewController = self.splitViewController.viewControllers[1];
   
+    [[self.searchText.rac_textSignal
+      map:^id(NSString *text) {
+          return [self isValidSearchText:text] ?
+          [UIColor whiteColor] : [UIColor yellowColor];
+      }]
+     subscribeNext:^(UIColor *color) {
+         self.searchText.backgroundColor = color;
+     }];
+    
+    
+    self.accountStore = [[ACAccountStore alloc] init];
+    self.twitterAccountType = [self.accountStore
+                               accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    
+    
+    [[[[self requestAccessToTwitterSignal]
+       then:^RACSignal *{
+       //    @strongify(self)
+           return self.searchText.rac_textSignal;
+       }]
+      filter:^BOOL(NSString *text) {
+        //  @strongify(self)
+          return [self isValidSearchText:text];
+      }]
+     subscribeNext:^(id x) {
+         NSLog(@"%@", x);
+     } error:^(NSError *error) {
+         NSLog(@"An error occurred: %@", error);
+     }];
+    
+}
+
+
+- (BOOL)isValidSearchText:(NSString *)text {
+    return text.length > 2;
 }
 
 - (void)styleTextField:(UITextField *)textField {
@@ -36,6 +85,35 @@
   textFieldLayer.borderColor = [UIColor grayColor].CGColor;
   textFieldLayer.borderWidth = 2.0f;
   textFieldLayer.cornerRadius = 0.0f;
+}
+
+
+- (RACSignal *)requestAccessToTwitterSignal {
+    
+    // 1 - define an error
+    NSError *accessError = [NSError errorWithDomain:RWTwitterInstantDomain
+                                               code:RWTwitterInstantErrorAccessDenied
+                                           userInfo:nil];
+    
+    // 2 - create the signal
+    //@weakify(self)
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        // 3 - request access to twitter
+        //@strongify(self)
+        [self.accountStore
+         requestAccessToAccountsWithType:self.twitterAccountType
+         options:nil
+         completion:^(BOOL granted, NSError *error) {
+             // 4 - handle the response
+             if (!granted) {
+                 [subscriber sendError:accessError];
+             } else {
+                 [subscriber sendNext:nil];
+                 [subscriber sendCompleted];
+             }
+         }];
+        return nil;
+    }];
 }
 
 @end
